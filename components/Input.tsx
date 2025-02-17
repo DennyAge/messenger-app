24fb6fd66840baf3f38018f9bc0590f8e6c48039
DennyAge <DenX055@gmail.com>
2025-02-17 13:36:20 +0200

    add nextAuth

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/libs/utils";
 import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 
 interface InputProps {
