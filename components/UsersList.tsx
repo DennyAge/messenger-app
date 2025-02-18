810959c85fd2ce420ff71facecf291f02607274c
DennyAge <DenX055@gmail.com>
2025-02-18 21:12:41 +0200

    add Conversation route

--- components/UsersList.tsx
+++ components/UsersList.tsx
@@ -13,7 +13,7 @@ const UsersList = ({ users }: UsersListProps) => {
   return (
     <aside
       className="fixed inside-y-0 pb-20 lg:pb-0 lg:left-20 lg:w-80 lg:block
-    overflow-y-auto border-r border-gray-200 block w-full left-0"
+    overflow-y-auto border-r border-gray-200 block w-full h-full left-0"
     >
       <div className="px-5">
         <div className="flex-col">
